040b8e8f0e3d952308138ec18202e00702e32c22
lengthylyova <tss.edits@gmail.com>
2023-04-08 13:31:04 +0300

    Classes refactor

--- objects.py
+++ objects.py
@@ -3,6 +3,23 @@ from random import randint
 from settings import keys
 
 
+class Players():
+	def __init__(self):
+		self.all = {}
+
+
+	class Player:
+		def __init__(self, snake):
+			self.snake = snake
+
+
+	def add(self, connection_id, player):
+		self.all[connection_id] = player
+	
+	def get(self, connection_id):
+		return self.all[connection_id].snake.tail.arr
+
+
 class Game():
 	def __init__(self):
 		self.status = False
@@ -12,39 +29,40 @@ class Game():
 		if key == Key.enter:
 			self.status = True
 		elif key == Key.esc:
-			self.status = False
-
-
-class Head():
-	def __init__(self):
-		self.y = 0
-		self.x = 0
-		self.previous_position = [self.y, self.x]
-
-
-class Tail():
-	def __init__(self):
-		self.arr = []
-
-	def add(self):
-		self.arr.append([0, 0])
-		return True
-
-	def pop(self):
-		self.arr.pop()
-		return True
-
-	def update(self, head):
-		self.arr.pop()
-		prev_pos = head.previous_position
-		self.arr.insert(0, prev_pos)
+			self.status = False	
 
 
 class Snake():
 	def __init__(self, head, tail, direction):
 		self.head = head
 		self.tail = tail
-		self.move_direction = 'DOWN'
+		self.move_direction = direction
+
+
+	class Tail():
+		def __init__(self):
+			self.arr = []
+
+		def add(self):
+			self.arr.append([0, 0])
+			return True
+
+		def pop(self):
+			self.arr.pop()
+			return True
+
+		def update(self, head):
+			self.arr.pop()
+			prev_pos = head.previous_position
+			self.arr.insert(0, prev_pos)
+
+
+	class Head():
+		def __init__(self):
+			self.y = 0
+			self.x = 0
+			self.previous_position = [self.y, self.x]
+
 
 	def suicide(self):
 		head = self.head
@@ -52,10 +70,12 @@ class Snake():
 			return True
 		return False
 
+
 	def spawn(self, field_width, field_height):
 		self.head.y = randint(0, field_height-1)
 		self.head.x = randint(0, field_width-1)
 		self.tail.arr = []
+
 
 	def move_direction_change(self, key):
 		if key == keys['d'] and self.move_direction != 'LEFT':
@@ -73,10 +93,15 @@ class Apple():
 		self.y = 0
 		self.x = 0
 
-	def spawn(self, field_width, field_height, snake):
-		
-		not_able = [i for i in snake.tail.arr]
-		not_able.append([snake.head.y, snake.head.x])
+	def spawn(self, field_width, field_height, players:dict):
+		not_able = []
+		for player in players:
+			s = players[player].snake
+			not_able.append([s.head.y, s.head.x])
+			not_able.append([i for i in s.tail.arr])
+
+		self.y = randint(0, field_height-1)
+		self.x = randint(0, field_width-1)
 		while [self.y, self.x] in not_able:
 			self.y = randint(0, field_height-1)
 			self.x = randint(0, field_width-1)
@@ -87,23 +112,26 @@ class Field():
 		self.width = field_width
 		self.height = field_height
 		self.arr = []
+		self.empty = []
 
 	def build(self):
 		for y in range(self.height):
-			self.arr.append([])
+			self.empty.append([])
 			for x in range(self.width):
-				self.arr[y].append("")
+				self.empty[y].append(" - ")
 		return True
 
-	def paint(self, snake, apple):
-		for y in range(self.height):
-			for x in range(self.width):
-				if ([y, x] == [snake.head.y, snake.head.x]) or ([y, x] in snake.tail.arr):
-					self.arr[y][x] = " @ "
-				elif [y, x] == [apple.y, apple.x]:
-					self.arr[y][x] = " $ "	
-				else:
-					self.arr[y][x] = " - "
+	def paint(self, players=None, apple=None):
+		self.arr = self.empty
+		if players != None:
+			for player in players:
+				s = players[player].snake
+				self.arr[s.head.y, s.head.x] = ' @ '
+				for tail_part in s.tail.arr:
+					self.arr[tail_part[0]][tail_part[1]] = ' @ '
+		
+		if apple != None:
+			self.arr[apple.y][apple.x] = ' $ '
 
 
 	def to_string(self):
